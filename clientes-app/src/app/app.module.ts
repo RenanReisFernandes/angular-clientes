a82d818a6c3e2f643ad5e9c056771e984ce606c6
Renan Reis <renanmacfernandes@hotmail.com>
2025-04-17 10:18:40 -0300

    fixing routes

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {TemplateModule} from './template/template.module';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component'
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
