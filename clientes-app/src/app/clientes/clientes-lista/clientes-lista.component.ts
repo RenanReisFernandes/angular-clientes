7f0b035bf9ebd20652fdc71f636b2bfdc654bed9
Renan Reis <renanmacfernandes@hotmail.com>
2025-04-19 18:12:29 -0300

    client list component creation

---
+++ clientes-app/src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes-lista',
+  standalone: false,
+  templateUrl: './clientes-lista.component.html',
+  styleUrl: './clientes-lista.component.css'
+})
+export class ClientesListaComponent {
+
+}
